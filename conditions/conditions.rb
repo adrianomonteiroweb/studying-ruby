485a755b991f55f19f9e81fc3eecb6b864a78e2b
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-08-19 18:38:29 -0300

    FIX: comments

--- conditions/conditions.rb
+++ conditions/conditions.rb
@@ -2,15 +2,21 @@ print 'Digite um número: '
 
 age = gets.chomp.to_i
 
+# IF
+
 # if number > 2
 #   puts "#{number} é maior que 2"
 # end
+
+# UNLESS
 
 # unless number >= 2
 #   puts "#{number} é menor que 2"
 # else
 #   puts "#{number} é maior que 2"
 # end
+
+# CASE
 
 case age
 when 0..2
