a274c261bc684db3a497a7caff65a6eb0cbc1ab1
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-08-19 18:37:08 -0300

    NEW: conditions - case

--- conditions/conditions.rb
+++ conditions/conditions.rb
@@ -1,13 +1,24 @@
 print 'Digite um número: '
 
-number = gets.chomp.to_i
+age = gets.chomp.to_i
 
 # if number > 2
 #   puts "#{number} é maior que 2"
 # end
 
-unless number >= 2
-  puts "#{number} é menor que 2"
+# unless number >= 2
+#   puts "#{number} é menor que 2"
+# else
+#   puts "#{number} é maior que 2"
+# end
+
+case age
+when 0..2
+  puts "Baby"
+when 3..12
+  puts "Child"
+when 13..18
+  puts "Teenager"
 else
-  puts "#{number} é maior que 2"
+  puts "Adult"
 end
