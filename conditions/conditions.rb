0a4eb59beb3019ad6d384e5fa96438af07f9bad3
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-08-19 18:30:23 -0300

    NEW: conditions - unless

--- conditions/conditions.rb
+++ conditions/conditions.rb
@@ -2,6 +2,12 @@ print 'Digite um número: '
 
 number = gets.chomp.to_i
 
-if number > 2
+# if number > 2
+#   puts "#{number} é maior que 2"
+# end
+
+unless number >= 2
+  puts "#{number} é menor que 2"
+else
   puts "#{number} é maior que 2"
 end
