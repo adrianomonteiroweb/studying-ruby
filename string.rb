acf5c63161b6435413f386bfc1c93a8148407699
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-08-20 17:26:41 -0300

    NEW: strings

---
+++ string.rb
@@ -0,0 +1,12 @@
+first_name = "Adriano"
+last_name = 'Monteiro'
+
+puts first_name.class
+puts last_name.class
+
+puts first_name.object_id # memory identificator
+
+puts first_name << last_name # first_name modify
+puts first_name + last_name
+
+puts "Interpolando #{first_name} #{last_name}."
