08884c6b307a6a43a7cf955899e9934a2d9d06a1
Adriano monteiro <adrianomonteiroweb@gmail.com>
2022-08-20 21:12:43 -0300

    NEW: hereditage

---
+++ heregitage.rb
@@ -0,0 +1,37 @@
+class Peaple
+  attr_accessor :name, :email
+end
+
+class Pf < Peaple
+  attr_accessor :cpf
+
+  def speak(text = "...")
+    text
+  end
+end
+
+class Pj < Peaple
+  attr_accessor :cnpj
+
+  def pay_supplier
+    puts "Paying supplier..."
+  end
+end
+
+p1 = Peaple.new
+puts p1.name = "Adriano"
+puts p1.email = "adriano@email.com"
+
+puts "---------------------"
+
+p2 = Pf.new
+puts p2.name = "Neto"
+puts p2.email = "neto@email.com"
+puts p2.speak("P2")
+
+puts "---------------------"
+
+p3 = Pj.new
+puts p3.name = "Ale"
+puts p3.email = "ale@email.com"
+puts p3.pay_supplier
